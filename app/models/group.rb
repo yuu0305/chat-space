09c85c2c149b34d6f934f111576e497254b7f494
yuu0305 <segyayuu8@gmail.com>
2019-04-16 20:55:06 +0900

    edit3
    dependent: :destroy

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
   has_many :messages, dependent: :destroy
-  has_many :users, through: :group_users, dependent: :destroy
+  has_many :users, through: :group_users
 
   validates :name, presence: true
 
