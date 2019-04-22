9ee05ad524a0e8cf72b069ac7f9ead52d543e38a
yuu0305 <segyayuu8@gmail.com>
2019-04-22 20:32:12 +0900

    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
+set :application, "chat-space"
 
 
 
@@ -13,7 +13,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/segawa_key_pair.pem']
+                  keys: ['segawa_key_pair']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
